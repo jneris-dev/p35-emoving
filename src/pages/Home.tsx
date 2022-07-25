edce405eff6d208e4a14a5e122d76b7d64153a0d
jneris-dev <site@jneris.com.br>
2022-07-25 14:55:01 -0300

    Disabled Loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,8 +19,6 @@ export function Home() {
     const params = useParams<SlugParams>();
     const { theme } = useTheme();
 
-    console.log(user)
-
     const [loading, setLoading] = useState(true);
     const [openMenu, setOpenMenu] = useState(
         isTablet || isMobile ? false : true
@@ -33,19 +31,13 @@ export function Home() {
     }, [user]);
 
     return (
-        <>
-            {!loading ?
-                <main className="home-main w-full flex items-stretch relative overflow-hidden">
-                    <Sidebar stateMenu={openMenu} switchMenu={setOpenMenu} paramsSlug={params.slug} />
-                    {params.slug === 'cliente' || params.slug === undefined ?
-                        <Cliente stateMenu={openMenu} switchMenu={setOpenMenu} theme={theme} />
-                        :
-                        <Comercial stateMenu={openMenu} switchMenu={setOpenMenu} theme={theme} />
-                    }
-                </main>
+        <main className="home-main w-full flex items-stretch relative overflow-hidden">
+            <Sidebar stateMenu={openMenu} switchMenu={setOpenMenu} paramsSlug={params.slug} />
+            {params.slug === 'cliente' || params.slug === undefined ?
+                <Cliente stateMenu={openMenu} switchMenu={setOpenMenu} theme={theme} />
                 :
-                <Loading />
+                <Comercial stateMenu={openMenu} switchMenu={setOpenMenu} theme={theme} />
             }
-        </>
+        </main>
     )
 }
